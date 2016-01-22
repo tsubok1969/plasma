1194ebe0162bd4c6df0f28a0bee4228d568fd17c
Ken Tsubouchi <kent1@mac.com>
2016-01-22 19:24:14 +0900

    plasma.pyを少し書き足した

--- plasma.py
+++ plasma.py
@@ -1,10 +1,18 @@
-from const import Const
+class plasma():
+    def __init__(self):
+        from const import Const
+        self.const = Const()
 
-def ev2vel(species, energy):
-
-def vel2ev(species, velocity):
-    if species == 0:
-        mass = EMAS
-    else:
-        mass = PMAS
+    def ev2vel(species, energy):
+        if species == 0:
+            mass = self.const.EMAS
+        else:
+            mass = self.const.PMAS
+        return 
+    
+    def vel2ev(species, velocity):
+        if species == 0:
+            mass = self.const.EMAS
+        else:
+            mass = self.const.PMAS
 
