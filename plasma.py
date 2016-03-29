a6f686a58eb86e50f5c05ba2df0e04d78d7e9b16
tsubok1969 <kent1@mac.com>
2016-03-29 09:15:47 +0900

    Velocity-to-eV added

--- plasma.py
+++ plasma.py
@@ -3,7 +3,7 @@ from tool import Calc
 c = Calc()
 
 print ("Choose the quantity:")
-print ("1: Alfven velocity\n2: Cyclotron frequency\n3: Plasma frequency")
+print ("1: Alfven velocity\n2: Cyclotron frequency\n3: Plasma frequency\n4: Velocity to eV")
 quant = input()
 quant = int(quant)
 
@@ -15,7 +15,12 @@ elif quant == 2:
     species, mag = map(float, input("Input the plasma species (0:electron 1:proton) and the magnetic field magnitude (nT)\n").split())
     cfreq = c.cyclotron_frequency(species, mag*c.NANO) / c.KILO
     print('Omega_c = %f kHz' % cfreq)
-else:
+else quant == 3:
     species, rho = map(float, input("Input the plasma species (0:electron 1:proton) and its number density (/cc)\n").split())
     pfreq = c.plasma_frequency(species, rho/c.CENT**3) / c.KILO
     print('Omega_p = %f kHz' % pfreq)
+else:
+    species, vel = map(float, input("Input the plasma species (0:electron 1:proton) and the velocity (km/s)").split())
+    ene = c.vel2joule(species, vel*c.KILO)
+    ev = c.joule2ev(ene) / c.KILO
+    print('Energy = %f keV' % ev)
